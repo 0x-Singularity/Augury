f3d8ba2108652967112dd4acf56eb70d1c16203c
0xGrizzly <gmcguir2@students.kennesaw.edu>
2025-02-19 10:45:21 -0500

    changed comments

--- backend/main.go
+++ backend/main.go
@@ -11,21 +11,17 @@ import (
 func main() {
 	router := mux.NewRouter()
 
-	// Serve static files from the frontend/static directory
 	staticFileDirectory := http.Dir("../frontend/static")
 	staticFileHandler := http.StripPrefix("/static/", http.FileServer(staticFileDirectory))
 	router.PathPrefix("/static/").Handler(staticFileHandler).Methods("GET")
 
-	// Handle the homepage route
 	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// Parse the template file
 		tmpl, err := template.ParseFiles("templates/index.html")
 		if err != nil {
 			http.Error(w, "Could not load template: "+err.Error(), http.StatusInternalServerError)
 			return
 		}
 
-		// Execute (render) the template
 		err = tmpl.Execute(w, nil)
 		if err != nil {
 			http.Error(w, "Could not render template: "+err.Error(), http.StatusInternalServerError)
